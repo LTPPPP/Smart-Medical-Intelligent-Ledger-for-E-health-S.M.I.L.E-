05c7bfa5817c2e3085bb225929cd54bc48822b38
chinhwind <chinhbc.work@gmail.com>
2026-05-20 03:33:15 +0000

    feat(prescriptions): add prescription modules

---
+++ backend/service/clinical-emr-service/src/prescriptions/prescriptions.controller.ts
@@ -0,0 +1,66 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { PrescriptionsService } from './prescriptions.service';
+import { CreatePrescriptionDto } from './dto/create-prescription.dto';
+import { UpdatePrescriptionDto } from './dto/update-prescription.dto';
+
+@ApiTags('Prescriptions')
+@Controller('prescriptions')
+export class PrescriptionsController {
+  constructor(private readonly prescriptionsService: PrescriptionsService) {}
+
+  @Post()
+  create(@Body() createPrescriptionDto: CreatePrescriptionDto) {
+    return this.prescriptionsService.create(createPrescriptionDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.prescriptionsService.findAll();
+  }
+
+  @Get(':prescription_id')
+  findOne(@Param('prescription_id', ParseUUIDPipe) prescription_id: string) {
+    return this.prescriptionsService.findOne(prescription_id);
+  }
+
+  @Get('patient/:patient_id')
+  findByPatientId(@Param('patient_id', ParseUUIDPipe) patient_id: string) {
+    return this.prescriptionsService.findByPatientId(patient_id);
+  }
+
+  @Get('doctor/:doctor_id')
+  findByDoctorId(@Param('doctor_id', ParseUUIDPipe) doctor_id: string) {
+    return this.prescriptionsService.findByDoctorId(doctor_id);
+  }
+
+  @Get('record/:record_id')
+  findByRecordId(@Param('record_id', ParseUUIDPipe) record_id: string) {
+    return this.prescriptionsService.findByRecordId(record_id);
+  }
+
+  @Patch(':prescription_id')
+  update(
+    @Param('prescription_id', ParseUUIDPipe) prescription_id: string,
+    @Body() updatePrescriptionDto: UpdatePrescriptionDto,
+  ) {
+    return this.prescriptionsService.update(
+      prescription_id,
+      updatePrescriptionDto,
+    );
+  }
+
+  @Delete(':prescription_id')
+  remove(@Param('prescription_id', ParseUUIDPipe) prescription_id: string) {
+    return this.prescriptionsService.remove(prescription_id);
+  }
+}
