45ca219caafa50c4045d443d1896b21ddbaaedb0
hugebenevolence <nhantd.dev@gmail.com>
2026-07-02 18:04:12 +0700

    fix(clinical): lock issued prescription items

--- backend/service/clinical-emr-service/src/prescription-items/prescription-items.module.ts
+++ backend/service/clinical-emr-service/src/prescription-items/prescription-items.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PrescriptionItemsService } from './prescription-items.service';
 import { PrescriptionItemsController } from './prescription-items.controller';
 import { PrescriptionItemEntity } from './entities/prescription-item.entity';
+import { PrescriptionEntity } from '../prescriptions/entities/prescription.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PrescriptionItemEntity])],
+  imports: [TypeOrmModule.forFeature([PrescriptionItemEntity, PrescriptionEntity])],
   controllers: [PrescriptionItemsController],
   providers: [PrescriptionItemsService],
   exports: [PrescriptionItemsService],
