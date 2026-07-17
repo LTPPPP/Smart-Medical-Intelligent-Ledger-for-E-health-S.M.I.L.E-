5b1fb1ed4ef20142deed612f3724046ef0ec9423
chinhwind <chinhbc.work@gmail.com>
2026-07-17 15:31:14 +0000

    feat(fe): add examination page routes (T020)

---
+++ frontend/web/src/app/(pages)/examinations/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { ExaminationDetail } from '@/features/examination/components/ExaminationDetail';
+
+export default function ExaminationsPage() {
+  return <ExaminationDetail />;
+}
