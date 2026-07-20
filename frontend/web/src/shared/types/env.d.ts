c06b7d636d4be693c3640f170c5dcb9bd2a755e4
Aohkne <aohkne@gmail.com>
2026-07-20 17:56:28 +0700

    fix(endpoint): remove unessary

--- frontend/web/src/shared/types/env.d.ts
+++ frontend/web/src/shared/types/env.d.ts
@@ -1,15 +1,8 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    NEXT_PUBLIC_ACCOUNT_API_URL: string;
-    NEXT_PUBLIC_CLINIC_API_URL: string;
-    NEXT_PUBLIC_APPOINTMENT_API_URL: string;
-    NEXT_PUBLIC_PATIENT_MEDIA_RECORD_API_URL: string;
-    NEXT_PUBLIC_SCHEDULE_API_URL: string;
-    NEXT_PUBLIC_SERVICE_API_URL: string;
-    NEXT_PUBLIC_EXAMINATION_API_URL: string;
-    NEXT_PUBLIC_DENTAL_IMAGE_API_URL: string;
-    
+    NEXT_PUBLIC_GATEWAY_URL: string;
     NEXT_PUBLIC_API_TIMEOUT: string;
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: string;
     NODE_ENV: 'development' | 'production' | 'test';
   }
 }
