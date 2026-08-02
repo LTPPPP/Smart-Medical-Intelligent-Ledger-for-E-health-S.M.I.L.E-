422e3cb79db2e81b58bf05f43f30ad594a41062d
KHOA <aohkne@gmail.com>
2026-08-02 15:26:07 +0000

    feat(iam): audit Google sign-ins

--- backend/service/iam-service/src/auth-google/auth-google.module.ts
+++ backend/service/iam-service/src/auth-google/auth-google.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { AuditLogsModule } from '../audit-logs/audit-logs.module';
 import { AuthGoogleController } from './auth-google.controller';
 import { AuthGoogleService } from './auth-google.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, AuditLogsModule],
   controllers: [AuthGoogleController],
   providers: [AuthGoogleService],
   exports: [AuthGoogleService],
