ad4ef8370edc61571b99cf83ea8ebe9796edf072
KHOA <aohkne@gmail.com>
2026-08-02 15:25:46 +0000

    chore: remove obsolete architecture diagram

--- backend/service/clinical-emr-service/src/treatment-plans/entities/treatment-plan.entity.ts
+++ backend/service/clinical-emr-service/src/treatment-plans/entities/treatment-plan.entity.ts
@@ -16,7 +16,6 @@ import { PatientRepresentativeEntity } from '../../patient-representatives/entit
 import { Currency } from '../../utils/enums/currency.enum';
 import { PlanStatus } from '../../utils/enums/plan-status.enum';
 
-
 @Entity({ name: 'treatment_plans' })
 export class TreatmentPlanEntity {
   @PrimaryGeneratedColumn('uuid', { name: 'plan_id' })
